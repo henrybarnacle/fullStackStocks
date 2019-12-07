92f81273b23100c237071ad7639d9ddf44016dc9
henrybarnacle <henrybarnacle@yahoo.com>
2019-12-07 15:19:30 -0600

    Update Stock.ts

--- src/app/models/Stock.ts
+++ src/app/models/Stock.ts
@@ -1,16 +1,24 @@
 
 export class Stock {
-code: string;
-companyName: string;
-latestTime: string;
-open: number;
-date: string;
-latestPrice: number;
-week52High: number;
-week52Low: number;
-primaryExchange: string;
-sector: string;
-peRatio: number;
-avgTotalVolume: number;
-user: string;
+	code: string;
+	companyName: string;
+	latestTime: string;
+	open: number;
+	date: any;
+    latestPrice: number;
+    week52High: number; 
+    week52Low: number;
+    primaryExchange: string; 
+    sector: string; 
+    peRatio: number;
+    avgTotalVolume: number;
+    user: string;
+    currentPrice: number;
+}
+
+export class StockHistory {
+    day: number;
+    week: number;
+    month: number;
+    year: number;
 }
